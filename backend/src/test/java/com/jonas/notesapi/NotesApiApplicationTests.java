1b71a0d4177da52b6ae1192326e6a7e297e37234
JonasCandido <jonasdcandido210@gmail.com>
2026-01-04 19:59:26 -0300

    Do base structure for the project

---
+++ backend/src/test/java/com/jonas/notesapi/NotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.notesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
